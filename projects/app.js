9b34f2973bf176992be384572d98ff16c3639f7b
maximilian-hack <github@max-hack.de>
2023-09-22 16:30:32 +0200

    some changes

--- projects/app.js
+++ projects/app.js
@@ -1,25 +1,23 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ§$%&{[]}";
-
-let interval = null;
-
-document.querySelector("nav a").onmouseover = event => {  
+function hackedtext(target){
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ§$%&{[]}";
+  let interval = null;  
   let iteration = 0;
   
   clearInterval(interval);
   
   interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
+    target.innerText = target.innerText
       .split("")
       .map((letter, index) => {
         if(index < iteration) {
-          return event.target.dataset.value[index];
+          return target.dataset.value[index];
         }
       
         return letters[Math.floor(Math.random() * 34)]
       })
       .join("");
     
-    if(iteration >= event.target.dataset.value.length){ 
+    if(iteration >= target.dataset.value.length){ 
       clearInterval(interval);
     }
     
