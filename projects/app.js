539ccf78744ec394ea0e5959ac85a10c89c0fc9c
maximilian-hack <github@max-hack.de>
2023-09-26 19:53:06 +0200

    I JUST FOUND OUT HOW TO AUTO FORMAT!!!11!!
    BRO I JUST PRESSED ALT + SCHIFT + F AND EVREYTHING LOOKED BETTER!!11!!!

--- projects/app.js
+++ projects/app.js
@@ -1,26 +1,26 @@
-function hackedtext(target){
+function hackedtext(target) {
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ§$%&{[]}";
-  let interval = null;  
+  let interval = null;
   let iteration = 0;
-  
+
   clearInterval(interval);
-  
+
   interval = setInterval(() => {
     target.innerText = target.innerText
       .split("")
       .map((letter, index) => {
-        if(index < iteration) {
+        if (index < iteration) {
           return target.dataset.value[index];
         }
-      
+
         return letters[Math.floor(Math.random() * 34)]
       })
       .join("");
-    
-    if(iteration >= target.dataset.value.length){ 
+
+    if (iteration >= target.dataset.value.length) {
       clearInterval(interval);
     }
-    
+
     iteration += 1 / 2;
   }, 20);
 }
